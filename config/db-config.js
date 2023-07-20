7bfd74c6b7132704458f5b8b51c3faf3a48c862f
Wade Wilson <sugumarsg17@gmail.com>
2023-07-20 18:51:14 +0530

    Added db config

---
+++ config/db-config.js
@@ -0,0 +1,13 @@
+const config={
+  HOST:"localhost",
+  USER:"wade",
+  PASSWORD:"1717",
+  DB:"wade",
+  dialect:"postgres",
+  pool:{
+    max:5,
+    min:0,
+    acquire:30000,
+    idle:10000,
+  }
+}
